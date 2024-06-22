c4139e3fc44bac9d62354cac8bf23b7041ac2e23
Matiu <mateovranych@hotmail.com>
2024-06-22 12:34:03 -0300

    Token interceptor y guard funcionando correctamente

--- src/app/index/header/header.component.ts
+++ src/app/index/header/header.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CartService } from '../../../services/carrito.service';
 import { CarritoIndexComponent } from '../carrito-index/carrito-index.component';
 import { MatDialog } from '@angular/material/dialog';
+import { CuentasService } from '../../../services/cuentas.service';
+import { DashboardComponent } from '../../admin/dashboard/dashboard.component';
 
 
 @Component({
@@ -13,6 +15,7 @@ import { MatDialog } from '@angular/material/dialog';
   imports: [MatIconModule,
             RouterLink,
             CommonModule,
+            
           ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -21,8 +24,11 @@ export class HeaderComponent {
 
   cartCount = 0;
 
-  constructor(private cartService: CartService,
+  constructor(private cartService: CartService,    
     private dialog: MatDialog,
+    private router: Router,   
+     private cuentasService: CuentasService,
+
   ) {}
 
   ngOnInit(): void {
@@ -35,6 +41,24 @@ export class HeaderComponent {
       width: '500px'
     });
   }
+
+  redirectToProfile() {
+    console.log('isLoggedIn:', this.cuentasService.isLoggedIn());
+    if (this.cuentasService.isLoggedIn()) {
+      console.log('isAdmin:', this.cuentasService.isAdmin());
+      if (this.cuentasService.isAdmin()) {
+        this.router.navigate(['/admin']);
+      } else {
+        this.router.navigate(['']); // Cambia esto a la ruta de perfil de usuario normal
+      }
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+  logout() {
+    this.cuentasService.cerrarSesion('/login');
+  }
+  
 
 
 }
--- src/app/index/login/login.component.ts
+++ src/app/index/login/login.component.ts
@@ -54,6 +54,5 @@ export class LoginComponent implements OnInit{
         }
       },            
     })
-
   }
 }
